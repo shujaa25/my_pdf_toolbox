961ba398f40186060b15ac5a8df6b341f5d5044e
Shujaa Ahmad <shujaa25@gmail.com>
2022-12-18 23:14:30 +0530

    added merge

---
+++ java/com/ishujaa/my_pdf_toolbox/MergeController.java
@@ -0,0 +1,87 @@
+package com.ishujaa.my_pdf_toolbox;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ListView;
+import javafx.stage.FileChooser;
+
+import java.io.File;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class MergeController implements Initializable{
+    @FXML
+    ListView<String> listview;
+    ArrayList<String> fileNames;
+    ArrayList<File> fileList;
+    String saveLocation;
+
+    private void setFileNameItems(){
+        ObservableList<String> observableList = FXCollections.observableArrayList(fileNames);
+        listview.setItems(observableList);
+    }
+
+    @FXML
+    protected void btnAddClick(){
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Select PDF Files");
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("PDF files (*.pdf)", "*.pdf"));
+        try {
+            List<File> files = fileChooser.showOpenMultipleDialog(listview.getScene().getWindow());
+            for (File file : files) {
+                fileNames.add(file.getName());
+                fileList.add(file);
+            }
+            setFileNameItems();
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }
+    @FXML
+    protected void btnRemoveClick(){
+        try{
+            int index = listview.getSelectionModel().getSelectedIndex();
+            if(index > -1){
+                fileNames.remove(index);
+                fileList.remove(index);
+                setFileNameItems();
+            }
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }
+    @FXML
+    protected void btnMoveUpClick(){
+
+    }
+    @FXML
+    protected void btnMoveDownClick(){}
+    @FXML
+    protected void btnExportClick(){
+        try{
+            FileChooser fileChooser = new FileChooser();
+            fileChooser.setTitle("Select Save Location");
+            fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("PDF files (*.pdf)", "*.pdf"));
+            saveLocation = fileChooser.showSaveDialog(listview.getScene().getWindow()).getPath();
+
+            if(!saveLocation.isEmpty()){
+                MergeTask task = new MergeTask(saveLocation, fileList);
+                WaitBox.display(task);
+            }
+
+        }catch (Exception e){
+            System.out.println(e);
+        }
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+        fileList = new ArrayList<>();
+        fileNames = new ArrayList<>();
+        saveLocation = "";
+    }
+}
---
+++ java/com/ishujaa/my_pdf_toolbox/MergeTask.java
@@ -0,0 +1,35 @@
+package com.ishujaa.my_pdf_toolbox;
+
+import javafx.concurrent.Task;
+import org.apache.pdfbox.multipdf.PDFMergerUtility;
+
+import java.io.File;
+import java.util.List;
+
+public class MergeTask extends Task<Void> {
+
+    private String destFileName;
+    private List<File> files;
+
+    MergeTask(String destFileName, List<File> files){
+        this.destFileName = destFileName;
+        this.files = files;
+    }
+
+    @Override
+    protected Void call() throws Exception {
+
+        PDFMergerUtility utility = new PDFMergerUtility();
+        utility.setDestinationFileName(destFileName);
+        for(File file: files) utility.addSource(file);
+
+        utility.mergeDocuments();
+
+        return null;
+    }
+
+    @Override
+    protected void succeeded() {
+        super.succeeded();
+    }
+}
