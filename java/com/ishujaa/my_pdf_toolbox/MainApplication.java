05d45c6131810f966da892adc95efcc90b381036
Shujaa Ahmad <shujaa25@gmail.com>
2022-12-18 23:23:26 +0530

    added merge

--- java/com/ishujaa/my_pdf_toolbox/MainApplication.java
+++ java/com/ishujaa/my_pdf_toolbox/MainApplication.java
@@ -3,6 +3,7 @@ package com.ishujaa.my_pdf_toolbox;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.stage.Modality;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -14,6 +15,8 @@ public class MainApplication extends Application {
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("My PDF ToolBox");
         stage.setScene(scene);
+        stage.setMinWidth(800);
+        stage.setMinHeight(450);
         stage.show();
     }
 
